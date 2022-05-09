85a60bcd842db1ce1066983331f697b875002098
Rohith GS <rohith.gs@siemens.com>
2022-05-09 10:41:54 +0530

    Working Junit code after code fix.

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,24 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Statistics getStatistics(List<Integer> numbers) {
-        //implement the computation of statistics here
+    public static Stats getStatistics(List<Float> numbers) {
+        if(numbers==null || numbers.isEmpty()){
+            return new Stats(Float.NaN,Float.NaN,Float.NaN);
+        }
+        float avg = (float) numbers.stream().mapToDouble(num->num).average().getAsDouble();
+        float max = (float) numbers.stream().mapToDouble(num->num).max().getAsDouble();
+        float min = (float) numbers.stream().mapToDouble(num->num).min().getAsDouble();
+       return new Stats(avg,max,min);
+    }
+
+    static class Stats {
+        public float average;
+        public float max;
+        public float min;
+        public Stats(float average, float max, float min) {
+            this.average = average;
+            this.max = max;
+            this.min = min;
+        }
     }
 }
